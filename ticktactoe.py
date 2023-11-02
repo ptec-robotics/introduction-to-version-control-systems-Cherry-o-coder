2abb3ee15e71f1bbff889c35ed5e1aaa1a284b96
Cherry-o-coder <nichols.sebastian@stu.jamesirwin.org>
2023-11-02 09:38:24 -0600

    tic tak toe

--- ticktactoe.py
+++ ticktactoe.py
@@ -1,11 +1,11 @@
 BOARD_W = 3
 BOARD_H = 3
 
-NONE =" "
+EMPTY =" "
 X = "X"
 O = "O"
 
-board = [ NONE ] * BOARD_W *BOARD_H
+board = [ EMPTY ] * BOARD_W *BOARD_H
 
 def draw(board):
     for row in range(BOARD_H):
@@ -27,6 +27,49 @@ def draw(board):
 def put(board, where, what):
     row = ord(where[1]) - ord("1")
     col = ord(where[0]) - ord("A")
+    i = row * BOARD_W + col
+    board[i] = what
+
+def count(straight, what):
+    count=0
+    for s in straight:
+        if s == what:
+            count +=1
+    return count
+
+def at(row, col):
+    return row * BOARD_W + col
+
+def check_rows(board, length):
+    for row in range(BOARD_H):
+        straight = [
+              board[at(row, 0)],
+              board[at(row, 1)],
+              board[at(row, 2)]
+          ]
+        if count (straight, X) == 3: return X
+        if count (straight, O) == 3: return O
+    return None
+
+def check_cols(board):
+    for col in range(BOARD_W):
+        straight = [
+            board[at(0, col)],
+            board[at(1, col)],
+            board[at(2, col)]
+        ]
+        if count (straight, X) == 3: return X
+        if count (straight, O) == 3: return O
+    return None
+    pass
+      
+
+
+
+
+
+def x_turn():
     pass
 
-draw(board)
+def o_turn():
+    pass
